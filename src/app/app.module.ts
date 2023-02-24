8a317a5d49d07798f5d0b833fc904777b75f5b5a
Alvaro Mena <alvaro.mena@sofka.com.co>
2023-02-24 11:05:19 -0500

    Se crearon los componentes necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HistorialComponent } from './component/historial/historial.component';
+import { CarritoComponent } from './component/carrito/carrito.component';
+import { ProductoComponent } from './component/producto/producto.component';
+import { AgregarProductoComponent } from './component/agregar-producto/agregar-producto.component';
+import { ModificarProductoComponent } from './component/modificar-producto/modificar-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HistorialComponent,
+    CarritoComponent,
+    ProductoComponent,
+    AgregarProductoComponent,
+    ModificarProductoComponent
   ],
   imports: [
     BrowserModule,
